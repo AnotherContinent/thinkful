fb88a9d4f463fdfbd298b1e46de89c58957011e7
AnotherContinent <smithah4588@gmail.com>
2015-01-15 00:35:12 +0000

    added single post

--- thinkful/blog/blog/config.py
+++ thinkful/blog/blog/config.py
@@ -3,3 +3,4 @@ class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgresql://action:action@localhost:5432/blogful"
     DEBUG = True
     SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", "")
+    
--- thinkful/blog/manage.py
+++ thinkful/blog/manage.py
@@ -6,6 +6,12 @@ from blog import app
 from blog.models import Post
 from blog.database import session
 
+from getpass import getpass
+
+from werkzeug.security import generate_password_hash
+
+from blog.models import User
+
 manager = Manager(app)
 
 @manager.command
@@ -24,6 +30,34 @@ def seed():
         )
         session.add(post)
     session.commit()
+    
+@manager.command
+def adduser():
+    name = raw_input("Name: ")
+    email = raw_input("Email: ")
+    if session.query(User).filter_by(email=email).first():
+        print "User with that email address already exists"
+        return
+
+    password = ""
+    password_2 = ""
+    while not (password and password_2) or password != password_2:
+        password = getpass("Password: ")
+        password_2 = getpass("Re-enter password: ")
+    user = User(name=name, email=email,
+                password=generate_password_hash(password))
+    session.add(user)
+    session.commit()
+    
+from flask.ext.migrate import Migrate, MigrateCommand
+from blog.database import Base
+
+class DB(object):
+    def __init__(self, metadata):
+        self.metadata = metadata
+
+migrate = Migrate(app, DB(Base.metadata))
+manager.add_command('db', MigrateCommand)
 
 if __name__ == "__main__":
     manager.run()
