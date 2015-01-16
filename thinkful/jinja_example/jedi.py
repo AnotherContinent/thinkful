8bc5041831b903aaa0a94f68e97269ed657ae828
AnotherContinent <smithah4588@gmail.com>
2015-01-16 00:37:56 +0000

    Changes to Jedi name

--- thinkful/jinja_example/jedi.py
+++ thinkful/jinja_example/jedi.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 @app.route("/jedi/<first>/<last>")
 def jedi_name(first, last):
-  first3 = first[:3]
-  last2 = last[:2]
-  return "Your Jedi name is {}{}".format(first3, last2)
+  first2 = first[:2]
+  last3 = last[:3]
+  return "Your Jedi name is {}{}".format(last3, first2)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
