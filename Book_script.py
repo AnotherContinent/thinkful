0d50873a7ccc25ac5ffcae3b431197136fa124a7
AnotherContinent <smithah4588@gmail.com>
2015-05-23 15:10:25 +0000

    Clean up

--- Book_script.py
+++
@@ -1,7 +0,0 @@
-if __name__ == '__main__':
-  Bike_One = Bicycle("Caprica", 10, 200)
-  Bike_Two = Bicycle("Tauron", 40, 75)
-  Bike_Three = Bicycle("Aerilon", 20, 500)
-  Bike_Four = Bicycle("Geminon"30, 750)
-  Bike_Five = Bicycle("Sagitarron", 25, 300)
-  Bike_Six = Bicycle("Picon", 15, 100)
